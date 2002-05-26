6b685fda26e8a008cd45871f2c4daf9e8ab3b984
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2002-05-26 21:34:54 +0000

    Implemented sets

--- gencsources.sh
+++ gencsources.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 DIR="it/unimi/dsi/fastMaps"
-FILE=(Map HashMap)
+FILE=(Map HashMap Set HashSet)
 TYPE=(boolean byte short int long char float double Object)
 TYPE_CAP=(Boolean Byte Short Int Long Char Float Double Object)
 CLASS=(Boolean Byte Short Integer Long Character Float Double Object)
 
-for ((f=0; f<${#FILE[*]}; f++)); do
+for ((f=0; f<2; f++)); do
 	 for ((k=1; k<${#TYPE[*]}; k++)); do
 		  for ((v=0; v<${#TYPE[*]}; v++)); do
 				if (( k < $((${#TYPE[*]}-1)) || v < $((${#TYPE[*]}-1)) )); then
@@ -71,3 +71,35 @@ for ((f=0; f<${#FILE[*]}; f++)); do
 			 done
 	  done
 done
+
+for ((f=2; f<4; f++)); do
+	 for ((k=1; k<${#TYPE[*]}; k++)); do
+					 FILENAME=$DIR/${TYPE_CAP[$k]}${FILE[$f]}.c
+					 rm -f $FILENAME
+					 echo -e \
+"#assert keyclass(${CLASS[$k]})\n"\
+"#define KEY_TYPE ${TYPE[$k]}\n"\
+"#define KEY_TYPE_CAP ${TYPE_CAP[$k]}\n"\
+"#define KEY_CLASS ${CLASS[$k]}\n"\
+"#define WRITE_KEY_TYPE_CAP write${TYPE_CAP[$k]}\n"\
+"#define READ_KEY_TYPE_CAP read${TYPE_CAP[$k]}\n"\
+"#define KEY_TYPE_VALUE ${TYPE[$k]}Value\n"\
+"#define NEXT_KEY_TYPE_CAP next${TYPE_CAP[$k]}\n"\
+"#define TO_KEY_TYPE_CAP_ARRAY to${TYPE_CAP[$k]}Array\n"\
+"#define SET_NAME ${TYPE_CAP[$k]}Set\n\n"\
+"#define HASHSET_NAME ${TYPE_CAP[$k]}HashSet\n\n"\
+"#if #keyclass(Object)\n"\
+"#define KEY2TYPE(x) (x)\n"\
+"#define KEY2OBJ(x) (x)\n"\
+"#define HASH(x) (x == null ? 0 : x.hashCode())\n"\
+"#define KEY_EQUAL(x,y) ((x) == (y) || ((x) != null && (x).equals((y))))\n"\
+"#else\n"\
+"#define KEY2TYPE(x) (((KEY_CLASS)(x)).KEY_TYPE_VALUE())\n"\
+"#define KEY2OBJ(x) (new KEY_CLASS(x))\n"\
+"#define HASH(x) (x)\n"\
+"#define KEY_EQUAL(x,y) ((x) == (y))\n"\
+"#endif\n\n"\
+"#include \"${FILE[$f]}.drv\"\n" >$FILENAME
+	  done
+done
+
--- test.sh
+++ test.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-FILE=(HashMap)
+FILE=(HashMap HashSet)
 TYPE=(boolean byte short int long char float double Object)
 TYPE_CAP=(Boolean Byte Short Int Long Char Float Double Object)
 CLASS=(Boolean Byte Short Integer Long Character Float Double Object)
 
 for ((t=10; t<1000000; t*=10)); do
-	 for ((f=0; f<1; f++)); do
-		  for ((k=1; k<${#TYPE[*]}; k++)); do
-				for ((v=0; v<${#TYPE[*]}; v++)); do
-					 if (( k < $((${#TYPE[*]}-1)) || v < $((${#TYPE[*]}-1)) )); then
-						  CLASSNAME=it.unimi.dsi.fastMaps.${TYPE_CAP[$k]}2${TYPE_CAP[$v]}${FILE[$f]}
-						  echo "Testing $CLASSNAME ($t elements)..."
-						  java -server -cp fastMaps.jar $CLASSNAME regressionTest $t
-					 fi
-				done
-		  done
-	 done
-
-#	 for ((f=2; f<4; f++)); do
-#		  for ((v=1; v<8; v++)); do
-#				CLASSNAME=$DIR/${TYPE_CAP[$v]}${FILE[$f]}
-#				echo "Testing $CLASSNAME ($t elements)..."
-#				java -server $CLASSNAME regressionTest $t
+#	 for ((f=0; f<1; f++)); do
+#		  for ((k=1; k<${#TYPE[*]}; k++)); do
+#				for ((v=0; v<${#TYPE[*]}; v++)); do
+#					 if (( k < $((${#TYPE[*]}-1)) || v < $((${#TYPE[*]}-1)) )); then
+#						  CLASSNAME=it.unimi.dsi.fastMaps.${TYPE_CAP[$k]}2${TYPE_CAP[$v]}${FILE[$f]}
+#						  echo "Testing $CLASSNAME ($t elements)..."
+#						  java -server -cp fastMaps.jar $CLASSNAME regressionTest $t
+#					 fi
+#				done
 #		  done
 #	 done
+
+	 for ((f=1; f<2; f++)); do
+		  for ((k=1; k<${#TYPE[*]}; k++)); do
+				CLASSNAME=it.unimi.dsi.fastMaps.${TYPE_CAP[$k]}${FILE[$f]}
+				echo "Testing $CLASSNAME ($t elements)..."
+				java -server -cp fastMaps.jar $CLASSNAME regressionTest $t
+		  done
+	 done
 done
