ac78d33939cb6d6729ab02174e2fec2d95a43a82
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2002-05-31 19:53:48 +0000

    Tweaking

--- test.sh
+++ test.sh
@@ -6,17 +6,17 @@ TYPE_CAP=(Boolean Byte Short Int Long Char Float Double Object)
 CLASS=(Boolean Byte Short Integer Long Character Float Double Object)
 
 for ((t=10; t<1000000; t*=10)); do
-#	 for ((f=0; f<1; f++)); do
-#		  for ((k=1; k<${#TYPE[*]}; k++)); do
-#				for ((v=0; v<${#TYPE[*]}; v++)); do
-#					 if (( k < $((${#TYPE[*]}-1)) || v < $((${#TYPE[*]}-1)) )); then
-#						  CLASSNAME=it.unimi.dsi.fastUtil.${TYPE_CAP[$k]}2${TYPE_CAP[$v]}${FILE[$f]}
-#						  echo "Testing $CLASSNAME ($t elements)..."
-#						  java -server -cp fastUtil.jar $CLASSNAME regressionTest $t
-#					 fi
-#				done
-#		  done
-#	 done
+	 for ((f=0; f<1; f++)); do
+		  for ((k=1; k<${#TYPE[*]}; k++)); do
+				for ((v=0; v<${#TYPE[*]}; v++)); do
+					 if (( k < $((${#TYPE[*]}-1)) || v < $((${#TYPE[*]}-1)) )); then
+						  CLASSNAME=it.unimi.dsi.fastUtil.${TYPE_CAP[$k]}2${TYPE_CAP[$v]}${FILE[$f]}
+						  echo "Testing $CLASSNAME ($t elements)..."
+						  java -server -cp fastUtil.jar $CLASSNAME regressionTest $t
+					 fi
+				done
+		  done
+	 done
 
 	 for ((f=1; f<2; f++)); do
 		  for ((k=1; k<${#TYPE[*]}; k++)); do
