c15ba06d405eff76a40e1dbdb729884e5c92b628
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2002-11-23 16:46:13 +0000

    Tweaking

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FILE=(HashSet HashMap)
+FILE=(HashSet TreeSet)
 TYPE=(boolean byte short int long char float double Object)
 TYPE_CAP=(Boolean Byte Short Int Long Char Float Double Object)
 CLASS=(Boolean Byte Short Integer Long Character Float Double Object)
@@ -9,8 +9,21 @@ lf=".75"
 
 if [ "$1" != "" ]; then lf=$1; fi
 
-for ((t=10; t<10000; t*=10)); do
-	 for ((f=1; f<2; f++)); do
+for ((t=10; t<1000; t*=10)); do
+	 for ((f=0; f<${#FILE[*]}; f++)); do
+		  for ((k=1; k<${#TYPE[*]}; k++)); do
+				CLASSNAME=it.unimi.dsi.fastUtil.${TYPE_CAP[$k]}${FILE[$f]}
+				echo "Testing $CLASSNAME ($t elements, load factor $lf)..."
+				java -server -cp fastUtil.jar $CLASSNAME regressionTest $t $lf
+		  done
+	 done
+
+done
+
+FILE=(HashMap TreeMap)
+
+for ((t=10; t<1000; t*=10)); do
+	 for ((f=1; f<${#FILE[*]}; f++)); do
 		  for ((k=1; k<${#TYPE[*]}; k++)); do
 				for ((v=1; v<${#TYPE[*]}; v++)); do
 					 CLASSNAME=it.unimi.dsi.fastUtil.${TYPE_CAP[$k]}2${TYPE_CAP[$v]}${FILE[$f]}
@@ -19,12 +32,4 @@ for ((t=10; t<10000; t*=10)); do
 				done
 		  done
 	 done
-	 for ((f=0; f<1; f++)); do
-		  for ((k=1; k<${#TYPE[*]}; k++)); do
-				CLASSNAME=it.unimi.dsi.fastUtil.${TYPE_CAP[$k]}${FILE[$f]}
-				echo "Testing $CLASSNAME ($t elements, load factor $lf)..."
-				java -server -cp fastUtil.jar $CLASSNAME regressionTest $t $lf
-		  done
-	 done
-
 done
