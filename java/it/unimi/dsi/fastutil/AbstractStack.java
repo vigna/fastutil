498825e3273bb76d17135857df1c31d22911b831
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2003-10-26 21:44:44 +0000

    New Stack interface and abstract implementations

---
+++ java/it/unimi/dsi/fastutil/AbstractStack.java
@@ -0,0 +1,51 @@
+/*		 
+ * fastutil: Fast & compact type-specific collections for Java
+ *
+ * Copyright (C) 2002, 2003 Sebastiano Vigna 
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package it.unimi.dsi.fastutil;
+
+
+/** An abstract class providing basic methods for implementing the {@link Stack} interface.
+ *
+ * <P>This class just defines {@link Stack#top()} as {@link Stack#peek(int) peek(0)}, and
+ * {@link Stack#peek(int)} as throwing an {@link UnsupportedOperationException}.
+ *
+ * Subclasses of this class may choose to implement just {@link Stack#push()},
+ * {@link Stack#pop()} and {@link Stack#isEmpty()}, or (but this is not
+ * required) go farther and implement {@link Stack#top()}, or even {@link
+ * Stack#peek(int)}.
+ */
+
+public abstract class AbstractStack implements Stack {
+
+	public Object top() {
+		return peek( 0 );
+	}
+
+	public Object peek( int i ) {
+		throw new UnsupportedOperationException();
+	}
+
+}
+
+// Local Variables:
+// mode: jde
+// tab-width: 4
+// End:
---
+++ java/it/unimi/dsi/fastutil/Stack.java
@@ -0,0 +1,79 @@
+/*		 
+ * fastutil: Fast & compact type-specific collections for Java
+ *
+ * Copyright (C) 2002, 2003 Sebastiano Vigna 
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package it.unimi.dsi.fastutil;
+
+/** A stack.
+ *
+ * <P>A stack must provide the classical {@link #push(Object)} and 
+ * {@link #pop()} operations, but may be also <em>peekable</em>
+ * to some extent: it may provide just the {@link #top()} function,
+ * or even a more powerful {@link #peek(int)} method that provides
+ * access to all elements on the stack (indexed from the top, which
+ * has index 0).
+ */
+
+public interface Stack {
+
+	/** Pushes the given object on the stack.
+	 *
+	 * @param o the object that will become the new top of the stack.
+	 */
+
+	void push( Object o );
+
+	/** Pops the top off the stack.
+	 *
+	 * @return the top of the stack.
+	 * @throws NoSuchElementException if the stack is empty.
+	 */
+
+	Object pop();
+
+	/** Checks whether the stack is empty.
+	 *
+	 * @return true if the stack is empty.
+	 */
+
+	boolean isEmpty();
+
+	/** Peeks at the top of the stack (optional operation).
+	 *
+	 * @return the top of the stack.
+	 * @throws NoSuchElementException if the stack is empty.
+	 */
+
+	Object top();
+
+	/** Peeks at an element on the stack (optional operation).
+	 *
+	 * @return the <code>i</code>-th element on the stack; 0 represents the top.
+	 * @throws IllegalArgumentException if the designated element does not exist..
+	 */
+
+	Object peek( int i );
+
+}
+
+// Local Variables:
+// mode: jde
+// tab-width: 4
+// End:
