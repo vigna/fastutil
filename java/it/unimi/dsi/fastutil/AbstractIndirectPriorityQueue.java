47c7ec06a0e0523f6db1f7f1c8f051b991fd8c2a
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2003-12-18 08:56:17 +0000

    Implemented wrappers

---
+++ java/it/unimi/dsi/fastutil/AbstractIndirectPriorityQueue.java
@@ -0,0 +1,47 @@
+/*		 
+ * fastdata: Fast & compact type-specific data structures for Java
+ *
+ * Copyright (C) 2003 Paolo Boldi and Sebastiano Vigna 
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package it.unimi.dsi.fastutil;
+
+/** An abstract class providing basic methods for implementing the {@link IndirectPriorityQueue} interface.
+ *
+ * <P>This class defines {@link #changed(int)},  {@link #remove(int)} and {@link #last()} as throwing an
+ * {@link UnsupportedOperationException}.
+ */
+
+public abstract class AbstractIndirectPriorityQueue implements IndirectPriorityQueue {
+
+	public int last() { throw new UnsupportedOperationException(); }
+
+	public void changed() { changed( first() ); }
+
+	public void changed( int index ) { throw new UnsupportedOperationException(); }
+
+	public void remove( int index ) { throw new UnsupportedOperationException(); }
+
+	public boolean isEmpty() { return size() == 0; }
+
+}
+
+// Local Variables:
+// mode: jde
+// tab-width: 4
+// End:
