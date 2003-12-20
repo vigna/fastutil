7bb7b9e4928f629e56c78e6fc62b3b262eda9718
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2003-12-20 18:26:53 +0000

    Tweaking

--- test.sh
+++ test.sh
@@ -58,7 +58,7 @@ for ((t=1; t<10000; t*=10)); do
 	done
     done
 
-    QUEUE=(HeapPriorityQueue HeapSemiIndirectPriorityQueue ArrayIndirectPriorityQueue HeapIndirectPriorityQueue HeapSesquiIndirectDoublePriorityQueue ArraySesquiIndirectDoublePriorityQueue)
+    QUEUE=(HeapPriorityQueue HeapSemiIndirectPriorityQueue ArrayIndirectPriorityQueue HeapIndirectPriorityQueue HeapSesquiIndirectDoublePriorityQueue ArrayIndirectDoublePriorityQueue)
 
     for ((f=0; f<${#QUEUE[*]}; f++)); do
 	l=${#TYPE[*]}
