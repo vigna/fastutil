ed9c9a66c3ce0c773c53fdec508c2981fcf3250a
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2010-09-23 19:16:54 +0000

    Tweaking

--- java/it/unimi/dsi/fastutil/BigSwapper.java
+++ java/it/unimi/dsi/fastutil/BigSwapper.java
@@ -1,6 +1,9 @@
 package it.unimi.dsi.fastutil;
 
-/** An object that can swap elements specified by longs. */
+/** An object that can swap elements specified by longs. 
+ * 
+ * @see BigArrays#quickSort(long, long, it.unimi.dsi.fastutil.longs.LongComparator, BigSwapper) 
+ */
 
 public interface BigSwapper {
 	/** Swaps the data at the given position.
--- java/it/unimi/dsi/fastutil/Swapper.java
+++ java/it/unimi/dsi/fastutil/Swapper.java
@@ -1,6 +1,9 @@
 package it.unimi.dsi.fastutil;
 
-/** An object that can swap elements specified by longs. */
+/** An object that can swap elements specified by longs. 
+ *
+ * @see Arrays#quickSort(int, int, it.unimi.dsi.fastutil.ints.IntComparator, Swapper)
+ */
 
 public interface Swapper {
 	/** Swaps the data at the given position.
