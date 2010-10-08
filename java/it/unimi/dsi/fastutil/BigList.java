80770ba73f0d8fa634c96183c1426d0f2f5fae97
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2010-10-08 20:49:11 +0000

    Tweaking

--- java/it/unimi/dsi/fastutil/BigList.java
+++ java/it/unimi/dsi/fastutil/BigList.java
@@ -35,7 +35,7 @@ public interface BigList<K> extends Collection<K>, Size64 {
 
 	/** Returns the element at the specified position.
 	 * 
-	 * @param index a position in the list.
+	 * @param index a position in the big list.
 	 * @return the element at the specified position.
 	 * @see List#get(int)
 	 */
@@ -43,33 +43,30 @@ public interface BigList<K> extends Collection<K>, Size64 {
 
 	/** Removes the element at the specified position.
 	 * 
-	 * @param index a position in the list.
+	 * @param index a position in the big list.
 	 * @return the element previously at the specified position.
 	 * @see List#remove(int)
 	 */
 	public K remove( long index );
 
-	/** Replaces the element at the specified position in this list with the specified element (optional operation). 
+	/** Replaces the element at the specified position in this big list with the specified element (optional operation). 
 	 * 
-	 * @param index a position in the list.
+	 * @param index a position in the big list.
 	 * @param element the element to be stored at the specified position.
 	 * @return the element previously at the specified positions.
 	 * @see List#set(int,Object)
 	 */
 	public K set( long index, K element );
 
-	/** Inserts the specified element at the specified position in this list (optional operation).
+	/** Inserts the specified element at the specified position in this big list (optional operation).
 	 * 
-	 * @param index a position in the list.
+	 * @param index a position in the big list.
 	 * @param element an element to be inserted.
 	 * @see List#add(int,Object)
 	 */
 	public void add( long index, K element );
 	
-	@Deprecated
-	public int size();
-	
-	/** Sets the size of this list.
+	/** Sets the size of this big list.
 	 *
 	 * <P>If the specified size is smaller than the current size, the last elements are
 	 * discarded. Otherwise, they are filled with 0/<code>null</code>/<code>false</code>.
@@ -79,44 +76,44 @@ public interface BigList<K> extends Collection<K>, Size64 {
 
 	void size( long size );
 
-	/** Inserts all of the elements in the specified collection into this list at the specified position (optional operation).
+	/** Inserts all of the elements in the specified collection into this big list at the specified position (optional operation).
 	 * 
 	 * @param index index at which to insert the first element from the specified collection.
-	 * @param c collection containing elements to be added to this list.
-	 * @return <code>true</code> if this list changed as a result of the call
+	 * @param c collection containing elements to be added to this big list.
+	 * @return <code>true</code> if this big list changed as a result of the call
 	 * @see List#addAll(int, Collection) 
 	 */	
 	public boolean addAll( long index, Collection<? extends K> c );
 
-	/** Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+	/** Returns the index of the first occurrence of the specified element in this big list, or -1 if this big list does not contain the element.
 	 * 
 	 * @param o the object to search for.
-	 * @return the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+	 * @return the index of the first occurrence of the specified element in this big list, or -1 if this big list does not contain the element.
 	 * @see List#indexOf(Object)
 	 */
 	public long indexOf( Object o );
 	
-	/** Returns the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element. 
+	/** Returns the index of the last occurrence of the specified element in this big list, or -1 if this big list does not contain the element. 
 	 * 
 	 * @param o the object to search for.
-	 * @return the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element.
+	 * @return the index of the last occurrence of the specified element in this big list, or -1 if this big list does not contain the element.
  	 * @see List#lastIndexOf(Object)
 	 */
 	public long lastIndexOf( Object o );
 	
-	/** Returns a list iterator over the elements in this list.
+	/** Returns a big-list iterator over the elements in this big list.
 	 * 
-	 * @return a list iterator over the elements in this list.
+	 * @return a big-list iterator over the elements in this big list.
 	 * @see List#listIterator()
 	 */
 
 	public BigListIterator<K> listIterator();
 
-	/** Returns a list iterator of the elements in this list, starting at the specified position in this list.
+	/** Returns a big-list iterator of the elements in this big list, starting at the specified position in this big list.
 	 * 
-	 * @param index index of first element to be returned from the list iterator.
-	 * @return a list iterator of the elements in this list, starting at the specified position in
-	 * this list.
+	 * @param index index of first element to be returned from the big-list iterator.
+	 * @return a big-list iterator of the elements in this big list, starting at the specified position in
+	 * this big list.
 	 * @see List#listIterator(int)
 	 */
 	public BigListIterator<K> listIterator( long index );
