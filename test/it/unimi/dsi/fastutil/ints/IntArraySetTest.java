011ee4d1805c73628d72905cc3a5ad62a098049e
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2010-11-01 22:37:16 +0000

    Tweaking

--- test/it/unimi/dsi/fastutil/ints/IntArraySetTest.java
+++ test/it/unimi/dsi/fastutil/ints/IntArraySetTest.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 import org.junit.Test;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 
 public class IntArraySetTest {
 	
@@ -18,45 +18,45 @@ public class IntArraySetTest {
 	public void testSet() {
 		for( int i = 1; i <= 1; i++ ) {
 			final IntArraySet s = i == 0 ? new IntArraySet() : new IntArraySet( new int[ i ] );
-			Assert.assertTrue( s.add( 1 ) );
-			Assert.assertEquals( 1 + i, s.size() );
-			Assert.assertTrue( s.contains( 1 ) );
-			Assert.assertTrue( s.add(  2  ) );
-			Assert.assertTrue( s.contains( 2 ) );
-			Assert.assertEquals( 2 + i, s.size() );
-			Assert.assertFalse( s.add( 1 ) );
-			Assert.assertFalse( s.remove( 3 ) );
-			Assert.assertTrue( s.add( 3 ) );
-			Assert.assertEquals( 3 + i, s.size() );
-			Assert.assertTrue( s.contains( 1 ) );
-			Assert.assertTrue( s.contains( 2 ) );
-			Assert.assertTrue( s.contains( 2 ) );
-			Assert.assertEquals( new IntOpenHashSet( i == 0 ? new int[] { 1, 2, 3 } : new int[] { 0, 1, 2, 3 } ), new IntOpenHashSet( s.iterator() ) );
-			Assert.assertTrue( s.remove( 3 ) );
-			Assert.assertEquals( 2 + i, s.size() );
-			Assert.assertTrue( s.remove( 1 ) );
-			Assert.assertEquals( 1 + i, s.size() );
-			Assert.assertFalse( s.contains( 1 ) );
-			Assert.assertTrue( s.remove( 2 ) );
-			Assert.assertEquals( 0 + i, s.size() );
-			Assert.assertFalse( s.contains( 1 ) );
+			assertTrue( s.add( 1 ) );
+			assertEquals( 1 + i, s.size() );
+			assertTrue( s.contains( 1 ) );
+			assertTrue( s.add(  2  ) );
+			assertTrue( s.contains( 2 ) );
+			assertEquals( 2 + i, s.size() );
+			assertFalse( s.add( 1 ) );
+			assertFalse( s.remove( 3 ) );
+			assertTrue( s.add( 3 ) );
+			assertEquals( 3 + i, s.size() );
+			assertTrue( s.contains( 1 ) );
+			assertTrue( s.contains( 2 ) );
+			assertTrue( s.contains( 2 ) );
+			assertEquals( new IntOpenHashSet( i == 0 ? new int[] { 1, 2, 3 } : new int[] { 0, 1, 2, 3 } ), new IntOpenHashSet( s.iterator() ) );
+			assertTrue( s.remove( 3 ) );
+			assertEquals( 2 + i, s.size() );
+			assertTrue( s.remove( 1 ) );
+			assertEquals( 1 + i, s.size() );
+			assertFalse( s.contains( 1 ) );
+			assertTrue( s.remove( 2 ) );
+			assertEquals( 0 + i, s.size() );
+			assertFalse( s.contains( 1 ) );
 		}
 	}
 	
 	@Test
 	public void testClone() {
 		IntArraySet s = new IntArraySet();
-		Assert.assertEquals( s, s.clone() );
+		assertEquals( s, s.clone() );
 		s.add( 0 );
-		Assert.assertEquals( s, s.clone() );
+		assertEquals( s, s.clone() );
 		s.add( 0 );
-		Assert.assertEquals( s, s.clone() );
+		assertEquals( s, s.clone() );
 		s.add( 1 );
-		Assert.assertEquals( s, s.clone() );
+		assertEquals( s, s.clone() );
 		s.add( 2 );
-		Assert.assertEquals( s, s.clone() );
+		assertEquals( s, s.clone() );
 		s.remove( 0 );
-		Assert.assertEquals( s, s.clone() );
+		assertEquals( s, s.clone() );
 	}
 
 	@Test
@@ -66,7 +66,7 @@ public class IntArraySetTest {
 		ObjectOutputStream oos = new ObjectOutputStream( baos );
 		oos.writeObject( s );
 		oos.close();
-		Assert.assertEquals( s, BinIO.loadObject( new ByteArrayInputStream( baos.toByteArray() ) ) );
+		assertEquals( s, BinIO.loadObject( new ByteArrayInputStream( baos.toByteArray() ) ) );
 		
 		s.add( 0 );
 		s.add( 1 );
@@ -75,6 +75,6 @@ public class IntArraySetTest {
 		oos = new ObjectOutputStream( baos );
 		oos.writeObject( s );
 		oos.close();
-		Assert.assertEquals( s, BinIO.loadObject( new ByteArrayInputStream( baos.toByteArray() ) ) );
+		assertEquals( s, BinIO.loadObject( new ByteArrayInputStream( baos.toByteArray() ) ) );
 	}
 }
--- test/it/unimi/dsi/fastutil/ints/IntArraysTest.java
+++ test/it/unimi/dsi/fastutil/ints/IntArraysTest.java
@@ -2,7 +2,7 @@ package it.unimi.dsi.fastutil.ints;
 
 import java.util.Arrays;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class IntArraysTest {
@@ -12,9 +12,9 @@ public class IntArraysTest {
 		int[] a = { 2, 1, 5, 2, 1, 0, 9, 1, 4, 2, 4, 6, 8, 9, 10, 12, 1, 7 }, b = a.clone(), sorted = a.clone();
 		Arrays.sort( sorted );
 		IntArrays.mergeSort( b );
-		Assert.assertArrayEquals( sorted, b );
+		assertArrayEquals( sorted, b );
 		IntArrays.mergeSort( b );
-		Assert.assertArrayEquals( sorted, b );
+		assertArrayEquals( sorted, b );
 		
 		final int[] d = a.clone();
 		IntArrays.mergeSort( d, new AbstractIntComparator() {
@@ -23,7 +23,7 @@ public class IntArraysTest {
 				return k1 - k2;
 			}
 		});
-		Assert.assertArrayEquals( sorted, d );
+		assertArrayEquals( sorted, d );
 
 		IntArrays.mergeSort( d, new AbstractIntComparator() {
 			@Override
@@ -31,7 +31,7 @@ public class IntArraysTest {
 				return k1 - k2;
 			}
 		});
-		Assert.assertArrayEquals( sorted, d );
+		assertArrayEquals( sorted, d );
 	}
 
 	@Test
@@ -39,9 +39,9 @@ public class IntArraysTest {
 		int[] a = { 2, 1, 5, 2, 1, 0, 9, 1, 4, 2, 4, 6, 8, 9, 10, 12, 1, 7 }, b = a.clone(), sorted = a.clone();
 		Arrays.sort( sorted );
 		IntArrays.quickSort( b );
-		Assert.assertArrayEquals( sorted, b );
+		assertArrayEquals( sorted, b );
 		IntArrays.quickSort( b );
-		Assert.assertArrayEquals( sorted, b );
+		assertArrayEquals( sorted, b );
 
 		final int[] d = a.clone();
 		IntArrays.quickSort( d, new AbstractIntComparator() {
@@ -50,13 +50,13 @@ public class IntArraysTest {
 				return k1 - k2;
 			}
 		});
-		Assert.assertArrayEquals( sorted, d );
+		assertArrayEquals( sorted, d );
 		IntArrays.quickSort( d, new AbstractIntComparator() {
 			@Override
 			public int compare( int k1, int k2 ) {
 				return k1 - k2;
 			}
 		});
-		Assert.assertArrayEquals( sorted, d );
+		assertArrayEquals( sorted, d );
 	}
 }
