bdfbce3baac61e89c1e28561d430c42941d598e3
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2010-11-01 22:23:22 +0000

    Finally fixed sorting, with tests

--- test/it/unimi/dsi/fastutil/ints/IntArraysTest.java
+++ test/it/unimi/dsi/fastutil/ints/IntArraysTest.java
@@ -9,10 +9,54 @@ public class IntArraysTest {
 	
 	@Test
 	public void testMergeSort() {
-		int[] a = { 2, 1, 5, 2, 1, 0, 9, 1, 4, 2, 4, 6, 8, 9, 10, 12, 1, 7 }, b = a.clone();
-		IntArrays.mergeSort( a );
-		Arrays.sort( b );
-		Assert.assertArrayEquals( b, a );
+		int[] a = { 2, 1, 5, 2, 1, 0, 9, 1, 4, 2, 4, 6, 8, 9, 10, 12, 1, 7 }, b = a.clone(), sorted = a.clone();
+		Arrays.sort( sorted );
+		IntArrays.mergeSort( b );
+		Assert.assertArrayEquals( sorted, b );
+		IntArrays.mergeSort( b );
+		Assert.assertArrayEquals( sorted, b );
+		
+		final int[] d = a.clone();
+		IntArrays.mergeSort( d, new AbstractIntComparator() {
+			@Override
+			public int compare( int k1, int k2 ) {
+				return k1 - k2;
+			}
+		});
+		Assert.assertArrayEquals( sorted, d );
+
+		IntArrays.mergeSort( d, new AbstractIntComparator() {
+			@Override
+			public int compare( int k1, int k2 ) {
+				return k1 - k2;
+			}
+		});
+		Assert.assertArrayEquals( sorted, d );
 	}
 
+	@Test
+	public void testQuickSort() {
+		int[] a = { 2, 1, 5, 2, 1, 0, 9, 1, 4, 2, 4, 6, 8, 9, 10, 12, 1, 7 }, b = a.clone(), sorted = a.clone();
+		Arrays.sort( sorted );
+		IntArrays.quickSort( b );
+		Assert.assertArrayEquals( sorted, b );
+		IntArrays.quickSort( b );
+		Assert.assertArrayEquals( sorted, b );
+
+		final int[] d = a.clone();
+		IntArrays.quickSort( d, new AbstractIntComparator() {
+			@Override
+			public int compare( int k1, int k2 ) {
+				return k1 - k2;
+			}
+		});
+		Assert.assertArrayEquals( sorted, d );
+		IntArrays.quickSort( d, new AbstractIntComparator() {
+			@Override
+			public int compare( int k1, int k2 ) {
+				return k1 - k2;
+			}
+		});
+		Assert.assertArrayEquals( sorted, d );
+	}
 }
