4e658cc1b6a9251b4311192875cf3365c9426d24
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2010-11-02 23:45:59 +0000

    Tweaking

--- test/it/unimi/dsi/fastutil/ints/IntBigArraysTest.java
+++ test/it/unimi/dsi/fastutil/ints/IntBigArraysTest.java
@@ -1,6 +1,8 @@
 package it.unimi.dsi.fastutil.ints;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
+import static it.unimi.dsi.fastutil.ints.IntBigArrays.set;
+import static it.unimi.dsi.fastutil.ints.IntBigArrays.get;
 
 import java.util.Arrays;
 
@@ -22,6 +24,34 @@ public class IntBigArraysTest {
 		IntBigArrays.quickSort( a );
 		assertArrayEquals( sorted, a );
 	}
+
+	private void testCopy( int n ) {
+		int[][] a = IntBigArrays.newBigArray( n );
+		for ( int i = 0; i < n; i++ ) set( a, i, i );
+		IntBigArrays.copy( a, 0, a, 1, n - 2 );
+		assertEquals( 0, a[ 0 ][ 0 ] );
+		for ( int i = 0; i < n - 2; i++ ) assertEquals( i,  get( a, i + 1 ) );
+		for ( int i = 0; i < n; i++ ) set( a, i, i );
+		IntBigArrays.copy( a, 1, a, 0, n - 1 );
+		for ( int i = 0; i < n - 1; i++ ) assertEquals( i + 1, get( a, i ) );
+		for ( int i = 0; i < n; i++ ) set( a, i, i );
+		int[] b = new int[ n ];
+		for ( int i = 0; i < n; i++ ) b[ i ] = i;
+		assertArrayEquals( a, IntBigArrays.wrap( b ) );
+	}
 	
-	
+	@Test
+	public void testCopy10() {
+		testCopy( 10 );
+	}
+
+	@Test
+	public void testCopy1000() {
+		testCopy( 1000 );
+	}
+
+	@Test
+	public void testCopy1000000() {
+		testCopy( 1000000 );
+	}
 }
