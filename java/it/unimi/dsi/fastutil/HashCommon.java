a5425c339657e7871dae839bdbad1398a63ecbd6
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2010-11-18 20:29:37 +0000

    Moved to Apache License 2.0

--- java/it/unimi/dsi/fastutil/HashCommon.java
+++ java/it/unimi/dsi/fastutil/HashCommon.java
@@ -1,25 +1,21 @@
 package it.unimi.dsi.fastutil;
 
 /*		 
- * fastutil: Fast & compact type-specific collections for Java
+ * Copyright (C) 2002-2010 Sebastiano Vigna 
  *
- * Copyright (C) 2002-2008 Sebastiano Vigna 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
  */
+
 
 /** Common code for all hash-based classes.
  *
