fbc467ca734382ce23edee533298f7d2c8f85ea1
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2010-11-18 18:47:51 +0000

    More tests

--- test/it/unimi/dsi/fastutil/ints/IntArrayFIFOQueueTest.java
+++ test/it/unimi/dsi/fastutil/ints/IntArrayFIFOQueueTest.java
@@ -9,7 +9,10 @@ public class IntArrayFIFOQueueTest {
 	@Test
 	public void testEnqueueDequeue() {
 		IntArrayFIFOQueue q = new IntArrayFIFOQueue();
-		for( int i = 0; i < 100; i++ ) q.enqueue( i );
+		for( int i = 0; i < 100; i++ ) {
+			q.enqueue( i );
+			assertEquals( i, q.lastInt() );
+		}
 		for( int i = 0; i < 100; i++ ) {
 			assertEquals( i, q.firstInt() );
 			assertEquals( i, q.dequeueInt() );
@@ -17,7 +20,10 @@ public class IntArrayFIFOQueueTest {
 		}
 
 		q = new IntArrayFIFOQueue( 10 );
-		for( int i = 0; i < 100; i++ ) q.enqueue( i );
+		for( int i = 0; i < 100; i++ ) {
+			q.enqueue( i );
+			assertEquals( i, q.lastInt() );
+		}
 		for( int i = 0; i < 100; i++ ) {
 			assertEquals( i, q.firstInt() );
 			assertEquals( i, q.dequeueInt() );
@@ -25,7 +31,10 @@ public class IntArrayFIFOQueueTest {
 		}
 
 		q = new IntArrayFIFOQueue( 200 );
-		for( int i = 0; i < 100; i++ ) q.enqueue( i );
+		for( int i = 0; i < 100; i++ ) {
+			q.enqueue( i );
+			assertEquals( i, q.lastInt() );
+		}
 		for( int i = 0; i < 100; i++ ) {
 			assertEquals( i, q.firstInt() );
 			assertEquals( i, q.dequeueInt() );
@@ -37,19 +46,28 @@ public class IntArrayFIFOQueueTest {
 	public void testMix() {
 		IntArrayFIFOQueue q = new IntArrayFIFOQueue();
 		for( int i = 0, p = 0; i < 200; i++ ) {
-			for( int j = 0; j < 20; j++ ) q.enqueue( j + i * 20 );
+			for( int j = 0; j < 20; j++ ) {
+				q.enqueue( j + i * 20 );
+				assertEquals( j + i * 20, q.lastInt() );
+			}
 			for( int j = 0; j < 10; j++ ) assertEquals( p++, q.dequeueInt() );
 		}
 		
 		q = new IntArrayFIFOQueue( 10 );
 		for( int i = 0, p = 0; i < 200; i++ ) {
-			for( int j = 0; j < 20; j++ ) q.enqueue( j + i * 20 );
+			for( int j = 0; j < 20; j++ ) {
+				q.enqueue( j + i * 20 );
+				assertEquals( j + i * 20, q.lastInt() );
+			}
 			for( int j = 0; j < 10; j++ ) assertEquals( p++, q.dequeueInt() );
 		}
 
 		q = new IntArrayFIFOQueue( 200 );
 		for( int i = 0, p = 0; i < 200; i++ ) {
-			for( int j = 0; j < 20; j++ ) q.enqueue( j + i * 20 );
+			for( int j = 0; j < 20; j++ ) {
+				q.enqueue( j + i * 20 );
+				assertEquals( j + i * 20, q.lastInt() );
+			}
 			for( int j = 0; j < 10; j++ ) assertEquals( p++, q.dequeueInt() );
 		}
 	}
@@ -57,9 +75,15 @@ public class IntArrayFIFOQueueTest {
 	@Test
 	public void testWrap() {
 		IntArrayFIFOQueue q = new IntArrayFIFOQueue( 30 );
-		for( int j = 0; j < 20; j++ ) q.enqueue( j );
+		for( int i = 0; i < 20; i++ ) {
+			q.enqueue( i );
+			assertEquals( i, q.lastInt() );
+		}
 		for( int j = 0; j < 10; j++ ) assertEquals( j, q.dequeueInt() );
-		for( int j = 0; j < 20; j++ ) q.enqueue( j );
+		for( int i = 0; i < 15; i++ ) {
+			q.enqueue( i );
+			assertEquals( i, q.lastInt() );
+		}
 		for( int j = 10; j < 20; j++ ) assertEquals( j, q.dequeueInt() );
 		for( int j = 0; j < 15; j++ ) assertEquals( j, q.dequeueInt() );
 	}
