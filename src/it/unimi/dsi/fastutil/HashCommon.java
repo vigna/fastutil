6f590b899091173f830ba43913ea937e63ece153
Sebastiano Vigna <sebastiano.vigna@unimi.it>
2010-11-21 14:42:11 +0000

    Tweaking

--- src/it/unimi/dsi/fastutil/HashCommon.java
+++ src/it/unimi/dsi/fastutil/HashCommon.java
@@ -35,6 +35,25 @@ public class HashCommon {
 
 	private HashCommon() {};
 
+	/** Avalanches the bits of an integer.
+	 * 
+	 * <p>This function implements the finalisation step of Austin Appleby's <a href="http://sites.google.com/site/murmurhash/">MurmurHash3</a>.
+	 * Its purpose is to avalanche the bits of the argument to within 0.25% bias. It is used, among other things, to scramble quickly (but deeply) the hash
+	 * values returned by {@link Object#hashCode()}.
+	 * 
+	 * @param x an integer.
+	 * @return a hash value with good avalanche properties.
+	 */	
+	public final static int avalanche( int x ) {
+		x ^= x >>> 16;
+		x *= 0x85ebca6b;
+		x ^= x >>> 13;
+		x *= 0xc2b2ae35;
+		x ^= x >>> 16;
+		return x;
+	}
+
+	
 	/** Returns the hash code that would be returned by {@link Float#hashCode()}.
 	 *
 	 * @return the same code as {@link Float#hashCode() new Float(f).hashCode()}.
@@ -54,11 +73,56 @@ public class HashCommon {
 		return (int)( l ^ ( l >>> 32 ) );
 	}
 
-	/** Returns the hash code that would be returned by {@link Long#hashCode()}.
+	/** Returns a hash code obtained by avalanching the 32 high bits of the argument, and XORing them with the 32 low bits.
+	 * 
+	 * <p><strong>Warning:</strong> this method up to <code>fastutil</code> 6.0.0
+	 * used to return the same code as {@link Long#hashCode() new Long(f).hashCode()}.
+	 * However, that hash code has worst statistical properties: I have personally once used a hash table of longs to keep track of pairs
+	 * of integers (in high/low bits), and that hash code (i.e., <code>(int)( l ^ ( l >>> 32 ) )</code>) would have given hash 0
+	 * to all pairs &lt;<var>x</var>, <var>x</var>>.
 	 *
-	 * @return the same code as {@link Long#hashCode() new Long(f).hashCode()}.
+	 * @return <code>(int)( l ^ avalanche( (int)( l >>> 32 ) ) </code>.
 	 */
 	final public static int long2int( final long l ) {
-		return (int)( l ^ ( l >>> 32 ) );
+		return (int)( l ^ avalanche( (int)( l >>> 32 ) ) );
+	}
+	
+	/** Return the least power of two greater than or equal to the specified value.
+	 * 
+	 * <p>Note that this function will return 1 when the argument is 0.
+	 * 
+	 * @param x an integer.
+	 * @return the least power of two greater than or equal to the specified value.
+	 */
+	public static int nextPowerOfTwo( int x ) {
+		if ( x == 0 ) return 1;
+		x--;
+		x |= x >> 1;
+		x |= x >> 2;
+		x |= x >> 4;
+		x |= x >> 8;
+		x |= x >> 16;
+		x++;
+		return x;
+	}
+
+	/** Return the least power of two greater than or equal to the specified value.
+	 * 
+	 * <p>Note that this function will return 1 when the argument is 0.
+	 * 
+	 * @param x a long integer.
+	 * @return the least power of two greater than or equal to the specified value.
+	 */
+	public static long nextPowerOfTwo( long x ) {
+		if ( x == 0 ) return 1;
+		x--;
+		x |= x >> 1;
+		x |= x >> 2;
+		x |= x >> 4;
+		x |= x >> 8;
+		x |= x >> 16;
+		x |= x >> 32;
+		x++;
+		return x;
 	}
 }
