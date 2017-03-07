9a0d5a19b1db46a8c558349ffc1dcd3cd42a046e
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2017-03-07 17:32:53 +0100

    Tests for RandomAccess

---
+++ test/it/unimi/dsi/fastutil/ints/IntListsTest.java
@@ -0,0 +1,32 @@
+package it.unimi.dsi.fastutil.ints;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.RandomAccess;
+
+import org.junit.Test;
+
+public class IntListsTest {
+	@Test
+	public void testRandomAccess() {
+		final IntList fakeList = new AbstractIntList() {
+			@Override
+			public int getInt(int index) {
+				return 0;
+			}
+			@Override
+			public int size() {
+				return 1;
+			}
+		};
+
+		assertFalse(IntLists.unmodifiable(fakeList) instanceof RandomAccess);
+		assertFalse(IntLists.synchronize(fakeList) instanceof RandomAccess);
+		assertFalse(IntLists.synchronize(fakeList, new Object()) instanceof RandomAccess);
+		
+		assertTrue(IntLists.unmodifiable(new IntArrayList()) instanceof RandomAccess);
+		assertTrue(IntLists.synchronize(new IntArrayList()) instanceof RandomAccess);
+		assertTrue(IntLists.synchronize(new IntArrayList(), new Object()) instanceof RandomAccess);
+	}
+}
