3d8b12fd1d791cd41500d29ad14f1fed3206964f
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2017-06-10 16:17:49 +0200

    Fixed (un)defaulting of methods in type-specific Function/Map instances

---
+++ test/it/unimi/dsi/fastutil/ints/Int2ObjectFunctionTest.java
@@ -0,0 +1,32 @@
+package it.unimi.dsi.fastutil.ints;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+@SuppressWarnings("rawtypes")
+public class Int2ObjectFunctionTest {
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testDefaultGenericMethods() {
+		final Integer zero = Integer.valueOf(0);
+		
+		@SuppressWarnings("serial")
+		Int2ObjectFunction<Object> f = new AbstractInt2ObjectFunction<Object>() {
+			@Override
+			public Object get(int key) {
+				return key == 0 ? zero : defRetValue;
+			}
+		};
+		
+		
+		final Object drv = new Object();
+		f.defaultReturnValue(drv);
+		assertSame(Integer.valueOf(0), f.get(0));
+		assertSame(drv, f.get(1));
+		assertSame(drv, f.get(Integer.valueOf(1)));
+		assertSame(zero, f.get(0));
+		assertSame(zero, f.get(Integer.valueOf(0)));
+	}
+}
