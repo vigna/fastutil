1b157819b9e90cc4cc3f9a33283e6b33858e0822
C. Sean Young <csyoung@google.com>
2020-11-03 18:04:37 -0600

    Add copyright header and make it clear it is intended migrate to real tests

--- test/it/unimi/dsi/fastutil/MainRunner.java
+++ test/it/unimi/dsi/fastutil/MainRunner.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017-2020 Sebastiano Vigna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package it.unimi.dsi.fastutil;
 
 import java.lang.reflect.Method;
@@ -10,8 +26,10 @@ import java.lang.reflect.Modifier;
  * be generated based on code generation flags. This class will query whether the class given has a
  * {@code main} method, and if so, calls it with the parameters given. The idea being to ensure
  * that these legacy tests get called by the unit tests.
+ * <br>It is intended that these tests will be converted into proper unit tests over time, but this
+ * can help bridge the gap until that is done. 
  *
- * <p>To jave these {@code main} method tests included in the code generation (as well as to generate
+ * <p>To have these {@code main} method tests included in the code generation (as well as to generate
  * some micro-benchmarks), pass {@code TEST=1} to the {@code make} command.
  *
  * @author C. Sean Young (csyoung@google.com)
