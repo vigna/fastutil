c04206d3a50887ec5feb316a886ec65ccc3edf6e
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2021-03-12 13:03:56 +0000

    Moved out references

--- split.sh
+++ split.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
 TMPFILE=$(mktemp)
 
-\ls -1 build/it/unimi/dsi/fastutil/*.class build/it/unimi/dsi/fastutil/io/*.class >$TMPFILE
-find build/it/unimi/dsi/fastutil/{ints,longs,doubles,objects} -iname \*.class -not -iname \*Short\* -not -iname \*Char\* -not -iname \*Boolean\* -not -iname \*Byte\* -not -iname \*Float\* >>$TMPFILE
-jdeps -v -R -e 'it\.unimi\.dsi\.fastutil($|.*)' -classpath build $(cat $TMPFILE) | awk '/build -> build/ {exit} { if (NF == 4) print "build/" gensub(/\./, "/", "g", $3) ".class" }' >>$TMPFILE
+\ls -1 build/it/unimi/dsi/fastutil/*.class build/it/unimi/dsi/fastutil/io/*.class | cut -d/ -f2- >$TMPFILE
+find build/it/unimi/dsi/fastutil/* -type f -iname \*.class -not -iname \*Short\* -not -iname \*Char\* -not -iname \*Boolean\* -not -iname \*Byte\* -not -iname \*Float\* -not -iname \*Reference\* | cut -d/ -f2- >>$TMPFILE
+jdeps -v -R -e 'it\.unimi\.dsi\.fastutil($|.*)' -classpath build $(while read F; do echo "build/$F"; done <$TMPFILE) | awk '/build -> build/ {exit} { if (NF == 4) print gensub(/\./, "/", "g", $3) ".class" }' >>$TMPFILE
 sort -u $TMPFILE >fastutil-core.txt
 
-find build/it/unimi/dsi/fastutil/{bytes,chars} -iname \*.class -not -iname \*Short\* -not -iname \*Boolean\* -not -iname \*Float\* >$TMPFILE
-jdeps -v -R -e 'it\.unimi\.dsi\.fastutil($|.*)' -classpath build $(cat $TMPFILE) | awk '/build -> build/ {exit} { if (NF == 4) print "build/" gensub(/\./, "/", "g", $3) ".class" }' >>$TMPFILE
-comm -2 -3 <(sort -u $TMPFILE) fastutil-core.txt >fastutil-bytechar.txt
+find build/it/unimi/dsi/fastutil/* -type f -iname \*.class -not -iname \*Short\* -not -iname \*Boolean\* -not -iname \*Float\* | cut -d/ -f2- >$TMPFILE
+jdeps -v -R -e 'it\.unimi\.dsi\.fastutil($|.*)' -classpath build $(while read F; do echo "build/$F"; done <$TMPFILE) | awk '/build -> build/ {exit} { if (NF == 4) print gensub(/\./, "/", "g", $3) ".class" }' >>$TMPFILE
+comm -2 -3 <(sort -u $TMPFILE) fastutil-core.txt >fastutil-refbytechar.txt
 
-comm -2 -3 <(find build/it/unimi/dsi/fastutil/ -iname \*.class | sort) <(sort -u fastutil-core.txt fastutil-bytechar.txt) >fastutil-boolshortfloat.txt
+comm -2 -3 <(find build/it/unimi/dsi/fastutil/ -type f -iname \*.class | cut -d/ -f2- | sort) <(sort -u fastutil-core.txt fastutil-refbytechar.txt) >fastutil-boolshortfloat.txt
