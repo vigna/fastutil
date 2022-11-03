1d4fe1c12a1c0eb4f13975176fa9ad461ba381c2
Richard Startin <richard.startin@datadoghq.com>
2022-11-03 10:57:38 +0000

    faster HashCommon.nextPowerOf2

--- src/it/unimi/dsi/fastutil/HashCommon.java
+++ src/it/unimi/dsi/fastutil/HashCommon.java
@@ -157,13 +157,7 @@ public class HashCommon {
 	 * @return the least power of two greater than or equal to the specified value.
 	 */
 	public static int nextPowerOfTwo(int x) {
-		if (x == 0) return 1;
-		x--;
-		x |= x >> 1;
-		x |= x >> 2;
-		x |= x >> 4;
-		x |= x >> 8;
-		return (x | x >> 16) + 1;
+		return 1 << (32 - Integer.numberOfLeadingZeros(x - 1));
 	}
 
 	/** Returns the least power of two greater than or equal to the specified value.
@@ -174,14 +168,7 @@ public class HashCommon {
 	 * @return the least power of two greater than or equal to the specified value.
 	 */
 	public static long nextPowerOfTwo(long x) {
-		if (x == 0) return 1;
-		x--;
-		x |= x >> 1;
-		x |= x >> 2;
-		x |= x >> 4;
-		x |= x >> 8;
-		x |= x >> 16;
-		return (x | x >> 32) + 1;
+		return 1L << (64 - Long.numberOfLeadingZeros(x - 1));
 	}
 
 
