311dbe93f47e7b5bbcd7e1d8a29e4b3cc20d98a7
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2026-07-22 16:07:29 +0200

    Bumped copyright dates

--- test/it/unimi/dsi/fastutil/ints/IntComparatorTest.java
+++ test/it/unimi/dsi/fastutil/ints/IntComparatorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2024 Barak Ugav and Sebastiano Vigna
+ * Copyright (C) 2003-2026 Barak Ugav and Sebastiano Vigna
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
